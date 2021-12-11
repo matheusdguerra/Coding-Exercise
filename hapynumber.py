f9d1f6baab2e62f240886eb948b0add12a1ced81
matheus.guerra <matheusguerra@outlook.com>
2021-12-11 12:59:14 -0300

    import hapynumber

--- hapynumber.py
+++ hapynumber.py
@@ -1,1 +1,13 @@
-assert
+
+def happy(number):
+    if number == 4:
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    assert happy(1)
+    assert happy(10)
+    assert happy(100)
+
+    assert not happy(4)

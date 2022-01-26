dd7611e5d55a65e419428c8f5d845ef7be2e33e9
matheus.guerra <matheusguerra@outlook.com>
2022-01-26 00:25:40 -0300

    Execução testes 25/01/2022

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -10,7 +10,9 @@ Regras FizzBuzz
 from functools import partial
 
 
-multiple_of = lambda base, num: num % base == 0
+def multiple_of(base, num): return num % base == 0
+
+
 multiple_of_5 = partial(multiple_of, 5)
 multiple_of_3 = partial(multiple_of, 3)
 
@@ -28,23 +30,43 @@ def robot(pos):
     return say
 
 
+# Testes no terminal
+# C:\Users\Matheus.Guerra.CETIL\PycharmProjects\fizzbuzz>python fizzbuzz.py -v
+# C:\Users\Matheus.Guerra.CETIL\PycharmProjects\fizzbuzz>python -m unittest
+
+# Teste após separar a classe do programa principal
+# C:\Users\Matheus.Guerra.CETIL\PycharmProjects\fizzbuzz>python -m unittest
+
+'''
+def assert_equal(result_robot, expected):
+    from sys import _getframe
+
+    msg = 'Fail: Line {} got {} expecting {}'
+
+    if not result_robot == expected:
+        current = _getframe()
+        caller = current.f_back
+        line_no = caller.f_lineno
+        print(msg.format(line_no, result_robot, expected))
+
+
 if __name__ == '__main__':
-    assert robot(1) == '1'
-    assert robot(2) == '2'
-    assert robot(4) == '4'
+    assert_equal(robot(1), '1')
+    assert_equal(robot(2), '2')
+    assert_equal(robot(4), '4')
 
-    assert robot(3) == 'fizz'
-    assert robot(6) == 'fizz'
-    assert robot(9) == 'fizz'
+    assert_equal(robot(3), 'fizz')
+    assert_equal(robot(6), 'fizz')
+    assert_equal(robot(9), 'fizz')
 
-    assert robot(5) == 'buzz'
-    assert robot(10) == 'buzz'
-    assert robot(20) == 'buzz'
+    assert_equal(robot(5), 'buzz')
+    assert_equal(robot(10), 'buzz')
+    assert_equal(robot(20), 'buzz')
 
-    assert robot(15) == 'fizzbuzz'
-    assert robot(30) == 'fizzbuzz'
-    assert robot(45) == 'fizzbuzz'
-
+    assert_equal(robot(15), 'fizzbuzz')
+    assert_equal(robot(30), 'fizzbuzz')
+    assert_equal(robot(45), 'fizzbuzz')
+'''
 
 """
 Testes no terminal
